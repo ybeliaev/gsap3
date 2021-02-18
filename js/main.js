f712c1ba5839d5e971d4afd7db91ff2cec76cfd9
Jorgen <ybeliaev@gmail.com>
2021-02-18 13:38:58 +0200

    add navNavigation menu bar hide and unhide

--- js/main.js
+++ js/main.js
@@ -12,13 +12,30 @@ function initNavigation(){
             }, 300)
         })
     })
+// stagger: 0.5 - 0.5 seconds between when each  element starts animating
+//  Basically autoAlpha will animate opacity from 1 to 0 then set the visibility property to hidden. When you animate autoAlpha to 0. autoAlpha will set visibility hidden to visible then animate opacity from 0 to 1 (or whatever value you set it to.)
+    function navNavigation(direction){
+        // direction - св-во объекта ScrollTrigger. =1 когда скролл вниз и запуск анимации, = -1 вверх 
+        console.log(direction);
+        let scrollingDown = direction === 1; // true or false
+        return gsap.to(mainNavLinks, {
+            duration: 1, 
+            stagger: 0.5, 
+            autoAlpha: () => scrollingDown ? 0 : 1 , 
+            y: () => scrollingDown ? 20 : 0,
+        })
+    }
+
     // code on 138 app.css
-    ScrollTrigger.create({
-        start: 100,
+    ScrollTrigger.create({ // ScrollTrigger - Animate anything on scroll
+        start: 100, //  starting scroll position (numeric, in pixels) from top
         toggleClass: {
             targets: "body", // на body вешается\убирается класс has-scrolled
             className: "has-scrolled"
         },
+        
+        onEnter: ({direction})=>navNavigation(direction), // запуск пропадания меню
+        onLeaveBack: ({direction})=>navNavigation(direction),
         markers: true
     })
 
